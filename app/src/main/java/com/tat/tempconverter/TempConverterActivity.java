98951213c93ef25737e0a0617e4365422f1dab52
Albert Tat <attat@ucsd.edu>
2017-04-07 04:27:19 -0700

    java implementation

--- app/src/main/java/com/tat/tempconverter/TempConverterActivity.java
+++ app/src/main/java/com/tat/tempconverter/TempConverterActivity.java
@@ -1,13 +1,85 @@
 package com.tat.tempconverter;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
-public class TempConverterActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+
+public class TempConverterActivity extends AppCompatActivity implements OnEditorActionListener {
+
+
+    private EditText FahrenHeitEditText;
+    private TextView mCelciusTV;
+    private SharedPreferences savedValues;
+    String fahrenheitString;
+    float celcius;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_converter);
+
+
+
+        FahrenHeitEditText = (EditText) findViewById(R.id.FahrenHeitEditText);
+        mCelciusTV = (TextView) findViewById(R.id.mCelciusTV);
+        //set listener
+        FahrenHeitEditText.setOnEditorActionListener(this);
+
+        //get shared preferences object
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        //implement listener
+        //String fahrenheitString;
+        //float celcius;
+
+        fahrenheitString = FahrenHeitEditText.getText().toString();
+        if(fahrenheitString == (""))
+        {
+            celcius = 0;
+        }
+        else
+        {
+            celcius = Float.parseFloat(fahrenheitString);
+        }
+        celcius = (celcius-32) * 5/9;
+
+        if(actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED)
+        {
+
+            mCelciusTV.setText(String.valueOf(celcius));
+        }
+        return false;
+    }
+
+    public void onPause()
+    {
+        //save instance variables
+        SharedPreferences.Editor editor = savedValues.edit();
+        editor.putString("fahrenheitString", fahrenheitString);
+        editor.putFloat(fahrenheitString, celcius);
+        editor.commit();
+
+        super.onPause();
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+
+        //get instance variables
+        fahrenheitString = savedValues.getString("fahrenheitString", "");
+        celcius = savedValues.getFloat("fahrenheitString", celcius);
     }
 }
